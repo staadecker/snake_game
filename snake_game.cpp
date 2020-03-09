3ba90212aa4cb9ea9ac3e6aa0da41cd13d484a6a
Martin Staadecker <machstg@gmail.com>
2020-03-09 17:07:45 -0400

    Create setup files

---
+++ snake_game.cpp
@@ -0,0 +1,12 @@
+﻿// snake_game.cpp : Defines the entry point for the application.
+//
+
+#include "snake_game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ snake_game.h
@@ -0,0 +1,8 @@
+﻿// snake_game.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
